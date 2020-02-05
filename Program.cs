721e79028f77af0ecaeea9dd85b693f15c4b85ee
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2020-02-04 19:53:40 -0500

    added solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double avg = 250.0;
+            double total = 250.0 *3;
+            
+            //candies to buy for clear and 50's
+            double candiesinclear50 = 0.1*total+total;
+            Console.WriteLine("numberof candies:{0}",candiesinclear50);
+
+            double candiesinfullmoon40 = 0.5*total+total;
+            Console.WriteLine("numberof candies:{0}", candiesinfullmoon40);
+            
+            double candiesinrainy70 = total-0.5*total;
+
+            Console.WriteLine("numberof candies:{0}",candiesinrainy70);
+            
+        }
+    }
+}
